6ddb97e2138b56cd152ae8dbbb8d4783ee152770
Franzi <3neu@informatik.uni-hamburg.de>
2017-06-30 11:13:42 +0200

    -initial commit

---
+++ Assets/Scripts/Creature.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Creature : MonoBehaviour {
+
+    public string typeName;
+    public string givenName;
+    public int level;
+
+    public int strenght;
+    public int defense;
+    public int magicalStrenght;
+    public int magicalDefense;
+    public int initiative;
+
+
+    public Job currentJob;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void rename(string _newname) {
+        givenName = _newname;
+    }
+
+    void catchCreature() {
+        if (true)
+        {
+            catchSuccess();
+        }
+        else {
+            catchFailed();
+        }
+    }
+
+    private void catchFailed()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void catchSuccess()
+    {
+        throw new NotImplementedException();
+    }
+}
