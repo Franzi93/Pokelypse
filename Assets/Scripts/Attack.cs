c124b324a12be6569f7a0b99c116b8baefab7ade
Franzi <3neu@informatik.uni-hamburg.de>
2017-07-01 19:08:00 +0200

    - test commit

--- Assets/Scripts/Attack.cs
+++ Assets/Scripts/Attack.cs
@@ -8,6 +8,7 @@ public enum attackType {PHYSICAL, MAGICAL }
 
 public class Attack : ScriptableObject {
 
+    public int id;
     public string attackName;
     public int hitChance;
     public int strength;
